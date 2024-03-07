721e2564dab94dfe3c6cd6cff57672957b8b2004
7meed <abdulhameed.khaled@everestminds.com>
2024-03-07 21:28:36 +0200

    setup chakra ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
-import "@/styles/globals.css";
+import {theme} from '../chakra/theme'
 import type { AppProps } from "next/app";
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (<ChakraProvider theme={theme}>
+    <Component {...pageProps} />
+  </ChakraProvider>);
 }
