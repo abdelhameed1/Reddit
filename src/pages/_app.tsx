392d8c85d041392a6112686d655996b94200dc98
7meed <abdulhameed.khaled@everestminds.com>
2024-03-08 15:35:00 +0200

    setup the layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import {theme} from '../chakra/theme'
 import type { AppProps } from "next/app";
 import { ChakraProvider } from '@chakra-ui/react'
+import Layout from '@/components/Layout/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (<ChakraProvider theme={theme}>
-    <Component {...pageProps} />
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+    
   </ChakraProvider>);
 }
