0837f5b47a8a3d67d278fb9c4b03165a67a2cb3a
7meed <abdulhameed.khaled@everestminds.com>
2024-03-09 21:24:18 +0200

    Add Button component styles to Chakra theme

--- src/chakra/theme.ts
+++ src/chakra/theme.ts
@@ -3,6 +3,9 @@ import { extendTheme } from "@chakra-ui/react"
 import '@fontsource/open-sans/300.css'
 import '@fontsource/open-sans/400.css'
 import '@fontsource/open-sans/700.css'
+
+import { Button } from './button'
+
 export const theme = extendTheme({
     colors: {
         brand: {
@@ -21,6 +24,6 @@ export const theme = extendTheme({
         })
     },
     components :{
-        
+        Button
     }
 })
