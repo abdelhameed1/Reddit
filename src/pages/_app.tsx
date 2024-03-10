6dff4bfe1ada234e4e73a9378cebb7ee54a25f03
7meed <abdulhameed.khaled@everestminds.com>
2024-03-10 22:16:31 +0200

    setup recoil on the website

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,17 @@ import {theme} from '../chakra/theme'
 import type { AppProps } from "next/app";
 import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '@/components/Layout/Layout';
+import { RecoilRoot } from 'recoil';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (<ChakraProvider theme={theme}>
+  return (
+  <RecoilRoot>
+    <ChakraProvider theme={theme}>
     <Layout>
       <Component {...pageProps} />
     </Layout>
     
-  </ChakraProvider>);
+  </ChakraProvider>
+  </RecoilRoot>
+  );
 }
