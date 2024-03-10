00b830ac674e22de82e63e001812169420453516
7meed <abdulhameed.khaled@everestminds.com>
2024-03-11 00:10:54 +0200

    Add AuthInputs component to AuthModal

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,7 +1,8 @@
-import { useDisclosure, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter } from '@chakra-ui/react';
+import { useDisclosure, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Flex } from '@chakra-ui/react';
 import React from 'react';
 import { useRecoilState } from 'recoil';
 import { AuthModalState } from '../../../atoms/authModalAtom';
+import AuthInputs from './AuthInputs';
 
 
 const AuthModal:React.FC= () => {
@@ -11,15 +12,23 @@ const AuthModal:React.FC= () => {
       setModalState(prev => ({...prev , open : false}))}
   return (
     <>
-     
-
       <Modal isOpen={modalState.open} onClose={handleClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          
+          <ModalHeader textAlign={'center'}>
+            {modalState.view === "login" && 'login' }
+            {modalState.view === 'signup' && 'Sign Up' }
+            {modalState.view === 'resetPassword' && 'Reset Password' }
+          </ModalHeader>
           <ModalCloseButton />
-          <ModalBody>
-            body
+          <ModalBody display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'} pb={6}>
+            <Flex direction={'column'} align={'center'} justify={'center'} width={'70%'}>
+             <AuthInputs/>
+             {/* <OAuthButtons/>
+              
+              <ResetPassword/> */}
+            </Flex>
           </ModalBody>
 
           
