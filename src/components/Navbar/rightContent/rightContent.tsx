4f0a98aee4b443a92a4fe09dfa76f9ac77d0d0a0
7meed <abdulhameed.khaled@everestminds.com>
2024-03-13 22:33:32 +0200

    Refactor Navbar rightContent component and update index page

--- src/components/Navbar/rightContent/rightContent.tsx
+++ src/components/Navbar/rightContent/rightContent.tsx
@@ -1,17 +1,19 @@
-import { Flex } from '@chakra-ui/react';
+import { Button, Flex } from '@chakra-ui/react';
 import React from 'react';
 import AuthButtons from './authButtons';
+import { signOut } from 'firebase/auth';
+import { auth } from '@/firebase/clientApp';
 
 type RightContentProps = {
-    
+    user : any
 };
 
-const RightContent:React.FC<RightContentProps> = () => {
+const RightContent:React.FC<RightContentProps> = ({user}) => {
     
     return (
         <>
             <Flex justify={'center'} align={'center'}>
-                <AuthButtons/>
+              {user ? <Button onClick={()=> signOut(auth)}>Logout</Button>:  <AuthButtons/>}
             </Flex>
         </>
     )
