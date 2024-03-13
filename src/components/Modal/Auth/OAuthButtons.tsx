0d02ce6a83f909e86e0692c63ad42b9bb8e5696f
7meed <abdulhameed.khaled@everestminds.com>
2024-03-13 21:58:54 +0200

    Add Google OAuth functionality and error handling

--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -1,17 +1,25 @@
-import { Button, Flex, Image } from '@chakra-ui/react';
+import { Button, Flex, Image, Text } from '@chakra-ui/react';
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithFacebook } from 'react-firebase-hooks/auth';
+import { auth } from '@/firebase/clientApp';
 
 
-const OAuthButtons:React.FC = () => {
-    
+const OAuthButtons: React.FC = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    //const [signInWithFacebook, userFb, loadingFb, errorFb] = useSignInWithFacebook(auth);
     return (
         <Flex direction={'column'} width={'100%'} mb={4}>
-
-            <Button variant={'oauth'} mb={2}>
+            <Button variant={'oauth'} mb={2} onClick={() => signInWithGoogle()}>
                 <Image src='/images/googlelogo.png' mr={4} height={'20px'}/>
                 Conitue with Google
-                </Button>
+            </Button>
+            {/* <Button variant={'oauth'} mb={2} onClick={() => signInWithFacebook()}>
+                <Image src='/images/FbLogo.png' mr={4} height={'20px'}/>
+                Conitue with Facebook
+            </Button> */}
+            {error && <Text>{error.message}</Text>}
         </Flex>
-    )
-}
+    );
+};
 export default OAuthButtons;
