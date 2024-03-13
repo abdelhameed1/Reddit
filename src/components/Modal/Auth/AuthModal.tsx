02b2def51d9342bd9d16f147193c1b8efcc69bf8
7meed <abdulhameed.khaled@everestminds.com>
2024-03-13 22:30:52 +0200

    Add useAuthState hook and handleClose function to AuthModal component

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -4,37 +4,45 @@ import { useRecoilState } from 'recoil';
 import { AuthModalState } from '../../../atoms/authModalAtom';
 import AuthInputs from './AuthInputs';
 import OAuthButtons from './OAuthButtons';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '@/firebase/clientApp';
 
 
-const AuthModal:React.FC= () => {
-    
-    const [modalState , setModalState] = useRecoilState(AuthModalState);
-    const handleClose = () => {
-      setModalState(prev => ({...prev , open : false}))}
+const AuthModal: React.FC = () => {
+
+  const [modalState, setModalState] = useRecoilState(AuthModalState);
+
+  const [user, loading, error] = useAuthState(auth)
+
+  const handleClose = () => { setModalState(prev => ({ ...prev, open: false })) }
+
+  React.useEffect(() => {
+    if (user) handleClose()
+  }, [user])
   return (
     <>
       <Modal isOpen={modalState.open} onClose={handleClose}>
         <ModalOverlay />
         <ModalContent>
-          
+
           <ModalHeader textAlign={'center'}>
-            {modalState.view === "login" && 'login' }
-            {modalState.view === 'signup' && 'Sign Up' }
-            {modalState.view === 'resetPassword' && 'Reset Password' }
+            {modalState.view === "login" && 'login'}
+            {modalState.view === 'signup' && 'Sign Up'}
+            {modalState.view === 'resetPassword' && 'Reset Password'}
           </ModalHeader>
           <ModalCloseButton />
           <ModalBody display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'} pb={6}>
             <Flex direction={'column'} align={'center'} justify={'center'} width={'70%'}>
-             <OAuthButtons />
-             <Text color={'grey.500'}>OR</Text>
-             <AuthInputs/>
-             
-              
-             {/* <ResetPassword/> */}
+              <OAuthButtons />
+              <Text color={'grey.500'}>OR</Text>
+              <AuthInputs />
+
+
+              {/* <ResetPassword/> */}
             </Flex>
           </ModalBody>
 
-          
+
         </ModalContent>
       </Modal>
     </>
