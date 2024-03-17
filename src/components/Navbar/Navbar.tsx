5114249794ac67358f6722972c199d3740b7b225
7meed <abdulhameed.khaled@everestminds.com>
2024-03-17 20:48:35 +0200

    Add CommunityMenu component to Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,19 +5,26 @@ import RightContent from './rightContent/rightContent';
 import AuthModal from '../Modal/Auth/AuthModal';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '@/firebase/clientApp';
-
+import CommunityMenu from './community/communityMenu';
 
 const Navbar:React.FC = () => {
-    const [user , loading , error] = useAuthState(auth)
+    const [user ] = useAuthState(auth)
     return (
-        <Flex bg="white" height={"44px"} padding={"6px 12px"}>
+        <Flex bg="white"
+         height={"44px"} 
+         padding={"6px 12px"}
+          >
             <Flex align={'center'}>
                 <Image height={'30px'} src='/images/redditFace.svg'/>
                 <Image height={'44px'} src='/images/redditText.svg' display={{base : 'none' , md : 'unset'}}/>
             </Flex>
+            <CommunityMenu/>
             <AuthModal/>
-            <SearchInput  />
-            <RightContent user={user}/>
+            <SearchInput user={user} />
+            <Flex style={{ marginLeft: 'auto' }} >
+                 <RightContent user={user} />
+            </Flex>
+           
         </Flex>
     )
 }
