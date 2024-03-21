4d63ac482f8d10bfe6a9fa4b5a73e85da8f9202f
7meed <abdulhameed.khaled@everestminds.com>
2024-03-21 19:54:24 +0200

    handle not existing community

--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -4,12 +4,16 @@ import { doc, getDoc } from 'firebase/firestore';
 import { GetServerSidePropsContext } from 'next';
 import React from 'react';
 import safeJsonStringify from 'safe-json-stringify';
+import NotFound from '@/components/Community/NotFound';
 type  CommunityPageProps = {
     communityData : Community 
 };
 
 const  CommunityPage:React.FC<CommunityPageProps> = ({communityData}) => {
     
+    if (!communityData) {
+        return  <NotFound/>  
+    }
     return (
         <>
         {communityData.id}
@@ -22,7 +26,7 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
         const communityDoc = await getDoc(communityDocRef);
         return {
             props: {
-                communityData: JSON.parse(safeJsonStringify({ id: communityDoc.id, ...communityDoc.data() }))
+                communityData: communityDoc.exists() ? JSON.parse(safeJsonStringify(communityDoc.data())) : ''
             }
         }
     }catch(e){
