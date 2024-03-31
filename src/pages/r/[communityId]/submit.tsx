43e634bedf8130072331c0bc2c8cf9b6d38d651c
7meed <abdulhameed.khaled@everestminds.com>
2024-03-31 22:34:00 +0200

    prevent unauthorized posting

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,19 +1,24 @@
 import PageContent from '@/components/Layout/PageContent';
 import NewPostForm from '@/components/Posts/NewPostForm';
+import { auth } from '@/firebase/clientApp';
 import { Box, Text } from '@chakra-ui/react';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 
-const SubmitPage:React.FC = () => {
-    
+const SubmitPage: React.FC = () => {
+    const [user] = useAuthState(auth);
     return (
         <PageContent>
-            <>
-            <Box p={'40px 0px'} borderBottom="1px solid" borderColor={'white'}>
-                <Text>Create a Post</Text>
-            </Box>
-            <NewPostForm/>
-            </>
+            {user ? (<>
+                    <Box p={'40px 0px'} borderBottom="1px solid" borderColor={'white'}>
+                        <Text>Create a Post</Text>
+                    </Box>
+                    <NewPostForm user={user} />
+                </>) : (<>
+                    <Text>Sign in to create a post</Text>
+                </>)}
+            
             <></>
         </PageContent>
     )
